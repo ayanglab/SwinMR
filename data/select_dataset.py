e0ba19723bd00a4043a69ae832a97815bfccddba
Jiahao Huang <55026816+kaaarho@users.noreply.github.com>
2022-07-05 17:02:05 +0100

    05/07/2022 update

--- data/select_dataset.py
+++ data/select_dataset.py
@@ -4,41 +4,17 @@
 def define_Dataset(dataset_opt):
     dataset_type = dataset_opt['dataset_type'].lower()
 
-    if dataset_type in ['miccai2013']:
-        from data.dataset_MICCAI2013 import DatasetMICCAI2013 as D
-
-    elif dataset_type in ['ccsagnpi']:
+    if dataset_type in ['ccsagnpi']:
         from data.dataset_CCsagnpi import DatasetCCsagnpi as D
 
     elif dataset_type in ['ccsagpi']:
         from data.dataset_CCsagpi import DatasetCCsagpi as D
-
-    elif dataset_type in ['ccaxinpi']:
-        from data.dataset_CCaxinpi import DatasetCCaxinpi as D
-
-    elif dataset_type in ['fastmri']:
-        from data.dataset_FastMRI import DatasetFastMRI as D
 
     elif dataset_type in ['brats17']:
         from data.dataset_BraTS17 import DatasetBraTS17 as D
 
     elif dataset_type in ['brats17_256']:
         from data.dataset_BraTS17_256 import DatasetBraTS17 as D
-
-    elif dataset_type in ['kaggle']:
-        from data.dataset_Kaggle import DatasetKaggle as D
-
-    elif dataset_type in ['kaggle_automask']:
-        from data.dataset_KaggleAutomask import DatasetKaggleAutomask as D
-
-    elif dataset_type in ['lascarqs2022']:
-        from data.dataset_LAScarQS2022 import DatasetLAScarQS2022 as D
-
-    elif dataset_type in ['lascarqs2022_224']:
-        from data.dataset_LAScarQS2022_224 import DatasetLAScarQS2022 as D
-
-    elif dataset_type in ['ilsvrc2012']:
-        from data.dataset_ILSVRC2012 import DatasetILSVRC2012 as D
 
     else:
         raise NotImplementedError('Dataset [{:s}] is not found.'.format(dataset_type))
--- models/select_model.py
+++ models/select_model.py
@@ -15,33 +15,6 @@ def define_Model(opt):
     elif model == 'swinmr_npi':
         from models.model_swinmr import MRI_SwinMR_NPI as M
 
-    elif model == 'swinmr_stgan':
-        from models.model_swinmr_stgan import MRI_STGAN as M
-
-    elif model == 'swinmr_eesgan':
-        from models.model_swinmr_eesgan import MRI_EESGAN as M
-
-    elif model == 'swinmr_tesgan':
-        from models.model_swinmr_tesgan import MRI_TESGAN as M
-
-    elif model == 'sdaut_npi':
-        from models.model_sdaut import MRI_SDAUT as M
-
-    elif model == 'sdaut_v2s':
-        from models.model_sdaut_v2s import MRI_SDAUTv2S as M
-
-    elif model == 'sdaut_v2c':
-        from models.model_sdaut_v2c import MRI_SDAUTv2C as M
-
-    elif model == 'unets':
-        from models.model_unet_s import MRI_UNetS as M
-
-    elif model == 'swinunets':
-        from models.model_swinunets import MRI_SwinUNetS as M
-
-    elif model == 'swinunetr':
-        from models.model_swinunetr import MRI_SwinUNetR as M
-
     else:
         raise NotImplementedError('Model [{:s}] is not defined.'.format(model))
 
